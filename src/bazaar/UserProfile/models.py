a1d98e7687cc66aa92733b2671c8839670a2f338
bkbranden <bk2dh@virginia.edu>
2019-01-30 22:13:45 -0500

    profile model done

--- src/bazaar/UserProfile/models.py
+++ src/bazaar/UserProfile/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    first_name = models.CharField(max_length=200, default='')
+    last_name = models.CharField(max_length=200, default='')
+    rating = models.DecimalField(max_digits=1, decimal_places=2)
+    description = models.TextField(default='')
+    education = models.CharField(max_length=200, default='')
+    zipcode = models.IntegerField(max_length=5)
