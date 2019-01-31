d9778faeb27e426756c44f579e081e03134179f9
bkbranden <bk2dh@virginia.edu>
2019-01-30 22:24:02 -0500

    profile migrate

--- src/bazaar/UserProfile/models.py
+++ src/bazaar/UserProfile/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Profile(models.Model):
     first_name = models.CharField(max_length=200, default='')
     last_name = models.CharField(max_length=200, default='')
-    rating = models.DecimalField(max_digits=1, decimal_places=2)
+    rating = models.DecimalField(max_digits=3, decimal_places=2)
     description = models.TextField(default='')
     education = models.CharField(max_length=200, default='')
     zipcode = models.IntegerField(max_length=5)
