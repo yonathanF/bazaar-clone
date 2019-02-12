726a4a9f44e0ad04732dbb812782e02f814005b9
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2019-02-12 01:08:47 -0500

    cleaned up the comment models so the expected endpoints work

--- src/bazaar/Comment/models.py
+++ src/bazaar/Comment/models.py
@@ -1,14 +1,10 @@
 from django.db import models
+
 from .StarConstants import Stars
 
-# Create your models here.
 
 class Comment(models.Model):
-	title = models.CharField(max_length = 100)
-	details = models.TextField()
-	stars = models.CharField(max_length=3, choices=Stars)
-	date_posted = models.DateTimeField(blank = True, null = True)
-
-	def publish(self):
-		self.date_posted = timezone.now()
-		self.save()
+    title = models.CharField(max_length=100)
+    details = models.TextField()
+    stars = models.CharField(max_length=3, choices=Stars)
+    date_posted = models.DateTimeField(auto_now=True)
--- src/bazaar/Comment/views.py
+++ src/bazaar/Comment/views.py
@@ -1,3 +1,85 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.core import serializers
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View
+from Post.models import Post
+from UserProfile.models import Profile
+
+from .forms import CreateCommentForm
+from .models import Comment
+
+
+def serialize_post(comment_id):
+    """
+    Return a JSON serialized post object
+    """
+    try:
+        comment = Comment.objects.filter(id=comment_id)
+        comment_json = json.loads(serializers.serialize("json", comment))
+        return JsonResponse({'comment': comment_json[0]['fields'],
+                             'id': comment_json[0]['pk']})
+    except:
+        return JsonResponse({'status': "Error. Couldn't find Comment."})
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CommentView(View):
+    """
+    Get and Update endpoint for comment
+    """
+    def get(self, request, comment_id):
+        return serialize_post(comment_id)
+
+    def post(self, request, comment_id):
+        try:
+            comment = Comment.objects.get(id=comment_id)
+        except Comment.DoesNotExist:
+            return JsonResponse({'status': "Error. Couldn't find Comment."})
+        comment_form = CreateCommentForm(request.POST, instance=comment)
+        if comment_form.is_valid():
+            new_comment = comment_form.save()
+            return JsonResponse({'created': serialize_post(new_comment)})
+        return JsonResponse({'error': comment_form.errors})
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CommentCreate(View):
+    """
+    Creates a post through the Post HTTP method
+    """
+
+    def post(self, request, post_id, user_id):
+
+        form = CreateCommentForm(request.POST)
+
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            try:
+                new_comment.post = Post.objects.get(pk=post_id)
+            except Post.DoesNotExist:
+                return JsonResponse({'status': "Error. Couldn't find Post."})
+            try:
+                new_comment.user = Profile.objects.get(pk=user_id)
+            except Profile.DoesNotExist:
+                return JsonResponse({'status': "Error. Couldn't find Profile."})
+
+            new_comment.save()
+
+            return serialize_post(new_comment.pk)
+        else:
+            return JsonResponse({'error': form.errors})
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CommentDelete(View):
+    """
+    DELETE endpoint for comment
+    """
+    def get(self, request, comment_id):
+        try:
+            Comment.objects.get(id=comment_id).delete()
+            return JsonResponse({'status': "deleted comment."})
+        except Comment.DoesNotExist:
+            return JsonResponse({'status': "Error. Couldn't find comment"})
