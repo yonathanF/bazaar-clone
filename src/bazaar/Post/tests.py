353317f0adb235a333609c68aefeefad06984aaf
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2019-02-15 00:39:36 -0500

    first tests

--- src/bazaar/Post/tests.py
+++ src/bazaar/Post/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from .models import Post
+from .PostChoiceConsts import Categories, Contact, Type
+from .views import serialize_post
+
+STATUS_OK = 200
+
+class PostSerializationTestCase(TestCase):
+    """
+    Tests that posts are seralized as expected
+    """
+    def setUp(self):
+        self.test_post = Post.objects.create(
+            title="Test Title",
+            details="This is a nice detail",
+            category=Categories[1][0],
+            preferred_contact=Contact[0][0],
+            deadline="2019-03-21",
+            zip_code=80012,
+            request_type=Type[0][0]
+        )
+
+    def test_valid_post_serializes(self):
+        response = serialize_post(self.test_post.pk)
+        self.assertEqual(STATUS_OK, response.status_code)
+        json_response = json.loads(response.json)
+
+        self.assertEquals(self.test_post.title, json_response['post']['title'])
