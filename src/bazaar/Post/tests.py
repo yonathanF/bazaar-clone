6444471935b19666b18a10aecbf8a66a7858de64
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2019-02-15 11:50:53 -0500

    passing test

--- src/bazaar/Post/tests.py
+++ src/bazaar/Post/tests.py
@@ -9,6 +9,7 @@ from .views import serialize_post
 
 STATUS_OK = 200
 
+
 class PostSerializationTestCase(TestCase):
     """
     Tests that posts are seralized as expected
@@ -27,6 +28,20 @@ class PostSerializationTestCase(TestCase):
     def test_valid_post_serializes(self):
         response = serialize_post(self.test_post.pk)
         self.assertEqual(STATUS_OK, response.status_code)
-        json_response = json.loads(response.json)
+
+        json_response = json.loads(response.content.decode('utf-8'))
 
         self.assertEquals(self.test_post.title, json_response['post']['title'])
+        self.assertEquals(self.test_post.deadline,
+                          json_response['post']['deadline'])
+        self.assertEquals(self.test_post.details,
+                          json_response['post']['details'])
+        self.assertEquals(self.test_post.category,
+                          json_response['post']['category'])
+        self.assertEquals(self.test_post.preferred_contact,
+                          json_response['post']['preferred_contact'])
+        self.assertEquals(self.test_post.zip_code,
+                          json_response['post']['zip_code'])
+
+
+
