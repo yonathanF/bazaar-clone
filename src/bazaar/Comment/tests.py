935bc07349b635ae0be1aecef9426c82fe3cf918
Simmy Bhatia <sk8wt@virginia.edu>
2019-02-17 23:54:12 -0500

    mostly configured the setup for comments

--- src/bazaar/Comment/tests.py
+++ src/bazaar/Comment/tests.py
@@ -1,12 +1,56 @@
+import datetime 
+import json
+
 from django.test import TestCase
 from .models import Comment
 from Post.models import Post
 from UserProfile.models import Profile
+from Post.PostChoiceConsts import Categories, Contact, Type
 from .StarConstants import Stars
-import datetime 
+from .views import serialize_post
+
+STATUS_OK = 200
+STATUS_NOTFOUND = 404
+STATUS_BAD = 400
 
 
 # Create your tests here.
+
+
+def create_test_comment():
+	post1 = Post.objects.create(
+			title = "Test Title",
+			details = "This is a nice detail",
+			category = Categories[1][0],
+			preferred_contact = Contact[0][0],
+			deadline = "2019-03-21",
+			zip_code = 80012,
+			request_type = Type[0][0]
+		)
+
+	user1 = Profile.objects.create(
+			first_name = "Sally".
+			last_name = "Sample",
+			rating = 3,
+			description = "This is a short bio", 
+			education = "Bachelor's Degrees",
+			zip_code = 22904
+		)
+
+
+    return Comment.objects.create(
+            title = "Test Comment",
+            details = "This is a comment about a particular post",
+            stars = Stars[1][0],
+            date_posted = "2019-03-21",
+            post = post1,
+            user = user1
+        )
+
+
+
+
+
 class CreateCommentTestCase(TestCase):
 	def setUp(self):
 		Comment.objects.create(
