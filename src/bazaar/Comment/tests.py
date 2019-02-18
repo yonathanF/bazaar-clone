1af625e3cd4c57d910ea1fe8641eae3edac0d514
Simmy Bhatia <sk8wt@virginia.edu>
2019-02-18 00:33:58 -0500

    added the create test case section of tests.py

--- src/bazaar/Comment/tests.py
+++ src/bazaar/Comment/tests.py
@@ -2,6 +2,8 @@ import datetime
 import json
 
 from django.test import TestCase
+from django.urls import reverse
+
 from .models import Comment
 from Post.models import Post
 from UserProfile.models import Profile
@@ -16,7 +18,6 @@ STATUS_BAD = 400
 
 # Create your tests here.
 
-
 def create_test_comment():
 	post1 = Post.objects.create(
 			title = "Test Title",
@@ -29,7 +30,7 @@ def create_test_comment():
 		)
 
 	user1 = Profile.objects.create(
-			first_name = "Sally".
+			first_name = "Sally",
 			last_name = "Sample",
 			rating = 3,
 			description = "This is a short bio", 
@@ -38,46 +39,92 @@ def create_test_comment():
 		)
 
 
-    return Comment.objects.create(
-            title = "Test Comment",
-            details = "This is a comment about a particular post",
-            stars = Stars[1][0],
-            date_posted = "2019-03-21",
-            post = post1,
-            user = user1
-        )
+	return Comment.objects.create(
+			title = "Test Comment",
+			details = "This is a comment about a particular post",
+			stars = Stars[1][0],
+			date_posted = "2019-03-21",
+			post = post1,
+			user = user1
+		)
+
+
+def comment_equals_form(comment, json_response):
+    """
+    Checks if the posts object is equal to the json object
+    """
+
+    if coment.title != json_response['title']:
+        return False
+
+    if coment.deadline != json_response['details']:
+        return False
+
+    if coment.details != json_response['stars']:
+        return False
+
+    if coment.category != json_response['date_posted']:
+        return False
+
+    if coment.preferred_contact != json_response['post']:
+        return False
+
+    if coment.zip_code != json_response['user']:
+        return False
+
+    return True
 
 
 
-
-
-class CreateCommentTestCase(TestCase):
+class CommentCreateTestCase(TestCase):
 	def setUp(self):
-		Comment.objects.create(
-			title="comment1", 
-			details="this is good", 
-			stars = Stars[0][0],
-			date_posted = "1998-03-21",
-			post_id = 1, 
-			user_id = 1)
-		Comment.objects.create(
-			title="comment2",  
-			details="this is bad", 
-			stars = Stars[0][0],
-			date_posted = "1998-03-21",
-			post_id = 2, 
-			user_id = 2)
+		self.test_comment = create_test_comment()
 
-	def test_comments_different_names(self):
-		comment1 = Comment.objects.get(title="comment1")
-		comment2 = Comment.objects.get(title="comment2")
-		self.assertNotEqual(comment1, comment2)
 
-	def test_comments_same_date(self):
-		comment1 = Comment.objects.filter(date_posted= datetime.date[1998, 3, 21])
-		print(comment1.count())
-		comment2 = Comment.objects.filter(date_posted = datetime.date[1998, 3, 21])
-		self.assertEqual(comment1, comment2)
+	def test_wellFormatted_form_creates(self):
+
+		post1 = Post.objects.create(
+				title = "Test Title",
+				details = "This is a nice detail",
+				category = Categories[1][0],
+				preferred_contact = Contact[0][0],
+				deadline = "2019-03-21",
+				zip_code = 80012,
+				request_type = Type[0][0]
+			)
+
+		user1 = Profile.objects.create(
+				first_name = "Sally",
+				last_name = "Sample",
+				rating = 3,
+				description = "This is a short bio", 
+				education = "Bachelor's Degrees",
+				zip_code = 22904
+			)
+
+		response = self.client.post(
+				reverse('createComment'),
+				{
+				'title' : "Test Comment",
+				'details' : "This is a comment about a particular post",
+				'stars' : Stars[1][0],
+				'date_posted' : "2019-03-21",
+				'post' : post1,
+				'user' : user1
+				})
+
+		self.assertEqual(STATUS_OK, response.status_code)
+
+	# def test_comments_different_names(self):
+	# 	comment1 = Comment.objects.get(title="comment1")
+	# 	comment2 = Comment.objects.get(title="comment2")
+	# 	self.assertNotEqual(comment1, comment2)
+
+	# def test_comments_same_date(self):
+	# 	comment1 = Comment.objects.filter(date_posted= datetime.date[1998, 3, 21])
+	# 	print(comment1.count())
+	# 	comment2 = Comment.objects.filter(date_posted = datetime.date[1998, 3, 21])
+	# 	self.assertEqual(comment1, comment2)
 
 
 
