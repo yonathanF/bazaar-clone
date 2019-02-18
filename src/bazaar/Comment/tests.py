e0fb4903b094b902f7721594337fcb2b0c6b9295
Simmy Bhatia <sk8wt@virginia.edu>
2019-02-18 02:52:50 -0500

    added functions for creating users and posts

--- src/bazaar/Comment/tests.py
+++ src/bazaar/Comment/tests.py
@@ -48,6 +48,27 @@ def create_test_comment():
 			user = user1
 		)
 
+def create_test_post():
+	return Post.objects.create(
+			title = "Test Title",
+			details = "This is a nice detail",
+			category = Categories[1][0],
+			preferred_contact = Contact[0][0],
+			deadline = "2019-03-21",
+			zip_code = 80012,
+			request_type = Type[0][0]
+		)
+
+def create_test_user():
+	return Profile.objects.create(
+				first_name = "Sally",
+				last_name = "Sample",
+				rating = 3,
+				description = "This is a short bio", 
+				education = "Bachelor's Degrees",
+				zip_code = 22904
+			)
+
 
 def comment_equals_form(comment, json_response):
     """
@@ -115,7 +136,7 @@ class CommentCreateTestCase(TestCase):
 
 		self.assertEqual(STATUS_OK, response.status_code)
 
-'''
+
 	def test_malformed_form_doesnt_create(self):
 
 		post1 = Post.objects.create(
@@ -149,7 +170,7 @@ class CommentCreateTestCase(TestCase):
 				})
 
 		self.assertEqual(STATUS_BAD, response.status_code)
-'''
+
 
 class CommentDeleteTestCase(TestCase):
 	"""
@@ -215,10 +236,12 @@ class CommentUpdateTestCase(TestCase):
     Tests the update endpoint for Post
     """
 
-    def setUp(self):
-        self.test_comment = create_test_post()
+	def setUp(self):
+		self.test_comment = create_test_comment()
+		self.test_post = create_test_post()
+		self.test_user = create_test_user()
 
- 	def test_malformed_input_doesnt_update(self):
+	def test_malformed_input_doesnt_update(self):
 		updated_deadline = "2014x09x02"
 
 		post1 = Post.objects.create(
@@ -247,14 +270,81 @@ class CommentUpdateTestCase(TestCase):
 					'title': self.test_comment.title,
 					'details': self.test_comment.details,
 					'stars': self.test_comment.stars,
-                    'date_posted': updated_deadline,
-                    'post': post1,
-                    'user': self.user1
+					'date_posted': updated_deadline,
+					'post': post1,
+					'user': user1
+				})
+
+		self.assertEqual(STATUS_BAD, response.status_code)
+
+
+    def test_existing_comment_updates(self):
+        updated_title = "Here's my new title"
+        updated_details = "And also, here's a new detail!"
+
+        # a post to this endpoint is an update
+        response = self.client.post(
+                reverse('viewComment', kwargs={'comment_id': self.test_comment.id}),
+				{
+					'title': self.test_comment.title,
+					'details': self.test_comment.details,
+					'stars': self.test_comment.stars,
+					'date_posted': updated_deadline,
+					'post': post1,
+					'user': user1
+				})
+
+        self.assertEqual(STATUS_OK, response.status_code)
+
+        json_response = json.loads(response.content.decode('utf-8'))
+        self.assertEquals(updated_zipcode,
+                          json_response['post']['zip_code'])
+
+        self.assertEquals(updated_deadline,
+                          json_response['post']['deadline'])
+
+    def test_nonexisting_post_doesnt_update(self):
+        updated_zipcode = 90421
+        updated_deadline = "2014-09-02"
+
+        # a post to this endpoint is an update
+        response = self.client.post(
+                reverse('viewPost', kwargs={'post_id': 400}),
+                {
+                    'title': self.test_post.title,
+                    'details': self.test_post.details,
+                    'category': self.test_post.category,
+                    'preferred_contact': self.test_post.preferred_contact,
+                    'deadline': updated_deadline,
+                    'zip_code': updated_zipcode,
+                    'request_type': self.test_post.request_type
                 })
 
-        self.assertEqual(STATUS_BAD, response.status_code)
+        self.assertEqual(STATUS_NOTFOUND, response.status_code)
 
 
+class PostSerializationTestCase(TestCase):
+    """
+    Tests that posts are seralized as expected
+    """
+    def setUp(self):
+        self.test_post = create_test_post()
+
+    def test_nonexisting_post(self):
+        response = serialize_post(400)
+        self.assertEqual(STATUS_NOTFOUND, response.status_code)
+
+        json_response = json.loads(response.content.decode('utf-8'))
+        self.assertIn('400', json_response['Status'])
+
+    def test_valid_post_serializes(self):
+        response = serialize_post(self.test_post.pk)
+        self.assertEqual(STATUS_OK, response.status_code)
+
+        json_response = json.loads(response.content.decode('utf-8'))
+
+        self.assertTrue(post_equals_form(
+                self.test_post, json_response['post']))
 
 
 
