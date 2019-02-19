0c1b8316433cf7698f49cbf6de4a87d2ff6fd620
bkbranden <bk2dh@virginia.edu>
2019-02-18 23:34:37 -0500

    postdetails experience API endpoints added

--- src/experience/experience/urls.py
+++ src/experience/experience/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/homepage/', include('homepage.urls')),
+    path('api/v1/postdetails/', include('postdetails.urls')),
 ]
