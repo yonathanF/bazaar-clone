d19431316c205c9e19abbc226764efa9d5442079
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2019-02-18 19:10:40 -0500

    added a nicer interface to the api wrapper

--- src/experience/apiwrapper/ApiWrapper.py
+++ src/experience/apiwrapper/ApiWrapper.py
@@ -16,6 +16,7 @@ class API(object):
     """
     def __init__(self, base_url=BASE_URL):
         self.base_url = base_url
+        self.STATUS_FAIL = 400
 
     def post(self, endpoint, data):
         url = self.base_url+endpoint
@@ -23,23 +24,82 @@ class API(object):
             response = requests.post(url, data=data)
             return response.status_code, response.json()
         except Exception as e:
-            return json.dumps(
+            return self.STATUS_FAIL, json.dumps(
                 {'Status': 'Failed to process request.[ %s ]' % (str(e))})
 
-    def get(self, endpoint, data):
+    def get(self, endpoint):
         url = self.base_url+endpoint
         try:
             response = requests.get(url)
             return response.status_code, response.json()
 
         except Exception as e:
-            return json.dumps(
+            return self.STATUS_FAIL, json.dumps(
                 {'Status': 'Failed to process request.[ %s ]' % (str(e))})
 
 
-class APIV1(API):
+class APIV1(object):
     """
     A more concrete implementation that is aware of the v1
     endpoints. Avoids coupling with the exact urls such as /posts
     """
-    pass
+
+    def __init__(self, server=None):
+        if server is None:
+            self.server = API()
+        else:
+            self.server = server
+
+        self.post_endpoint = '/post/'
+
+    def post_get(self, post_id):
+        """
+        Gets the post specified by post_id
+        """
+        url = self.post_endpoint+str(post_id)
+        response = self.server.get(url)
+        return response
+
+    def post_delete(self, post_id):
+        """
+        Deletes the post specified by the post_id
+        """
+        url = self.post_endpoint+"delete/"+str(post_id)
+        _, response = self.server.get(url)
+
+        return response
+
+    def post_update(self, post_id, title, details, category,
+                    preferred_contact, deadline, zip_code,
+                    request_type, user_id):
+        data = {
+                    'title': title,
+                    'details': details,
+                    'category': category,
+                    'preferred_contact': preferred_contact,
+                    'deadline': deadline,
+                    'zip_code': zip_code,
+                    'request_type': request_type,
+                    'user': user_id
+                }
+
+        url = self.post_endpoint+str(post_id)+"/"
+        _, response = self.server.post(url, data)
+        return response
+
+    def post_create(self, title, details, category, preferred_contact,
+                    deadline, zip_code, request_type, user_id):
+        data = {
+            'title': title,
+            'details': details,
+            'category': category,
+            'preferred_contact': preferred_contact,
+            'deadline': deadline,
+            'zip_code': zip_code,
+            'request_type': request_type,
+            'user': user_id
+        }
+
+        url = self.post_endpoint+"create/"
+        _, response = self.server.post(url, data)
+        return response
--- src/experience/apiwrapper/test_integration.py
+++ src/experience/apiwrapper/test_integration.py
@@ -1,8 +1,31 @@
+from unittest.mock import MagicMock
+
 from django.test import TestCase
 
-from .ApiWrapper import API
+from .ApiWrapper import API, APIV1
 
 STATUS_OK = 200
+
+
+class APIV1TestCase(TestCase):
+    """
+    Test cases for the v1 api wrapper
+    """
+
+    def setUp(self):
+        self.server = API()
+
+    def test_get_post(self):
+        self.server.get = MagicMock(return_value={'test': 1})
+        api = APIV1(self.server)
+
+        self.assertEquals(api.post_get(1), {'test': 1})
+
+    def test_post_post(self):
+        self.server.post = MagicMock(return_value={'test': 1})
+        api = APIV1(self.server)
+
+        self.assertEquals(api.post_create(1), {'test': 1})
 
 
 class ApiWrapperTestCase(TestCase):
@@ -47,9 +70,8 @@ class ApiWrapperTestCase(TestCase):
         self.assertEquals(STATUS_OK, status_code)
 
         get_url = '/profile/'+str(response['id'])
-        get_data = {}
 
-        status_code, response = self.api.get(get_url, get_data)
+        status_code, response = self.api.get(get_url)
 
         self.assertEquals(STATUS_OK, status_code)
 
