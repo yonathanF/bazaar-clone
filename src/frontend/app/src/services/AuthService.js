e482d49bd074aeaf0910874505e3693ba7585b8a
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2019-03-12 19:19:15 -0400

    Basic auth logic
    Added the based auth logic
    Re-organized the service dir
      - the user service will have functions for things
        like updating/accessing profile etc
      - the auth service should handle all things related to
        authentication

--- src/frontend/app/src/services/AuthService.js
+++ src/frontend/app/src/services/AuthService.js
@@ -1,4 +1,5 @@
 import { logout } from "./UserService";
+import { HTTP } from "../APIBase";
 
 function handleResponse(response) {
   return response.text().then(text => {
@@ -20,6 +21,41 @@ function handleResponse(response) {
   });
 }
 
+function logout() {
+  HTTP.get("logout/", {
+    params: {
+      token: localStorage.getItem("token")
+    }
+  }).then(localStorage.removeItem("token"));
+}
+
+function login(email, password) {
+  HTTP.post("login/", {
+    email: email,
+    password: password
+  })
+    .then(handleResponse)
+    .then(token => {
+      if (token) {
+        localStorage.setItem("token", JSON.stringify(token));
+      }
+    });
+}
+
+function register(firstname, lastname, email, password) {
+  HTTP.post("register/", {
+    firstname: firstname,
+    lastname: lastname,
+    email: email,
+    password: password
+  })
+    .then(handleResponse)
+    .then(token => {
+      if (token) {
+        localStorage.setItem("token", JSON.stringify(token));
+      }
+    });
+}
 export function isAuthenticated() {
   const token = localStorage.getItem("token");
   return !(!token || token.length === 0);
--- src/frontend/app/src/services/UserService.js
+++
@@ -1,9 +0,0 @@
-import { HTTP } from "../APIBase";
-
-function login(username, password) {}
-
-function register(firstname, lastname, email, password){}
-
-function logout() {}
-
-function getUserProfile() {}
