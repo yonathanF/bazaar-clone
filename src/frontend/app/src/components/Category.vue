86819938b93b10a400eeef944f4513fe0703150d
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2019-03-14 15:29:01 -0400

    cleaned up the ui for the homapge

--- src/frontend/app/src/components/Category.vue
+++ src/frontend/app/src/components/Category.vue
@@ -5,7 +5,12 @@
       row 
       >
       <v-flex> 
-	<h1 class="category"> {{categoryName}}</h1> 
+	<v-layout justify-end align-end>
+  <v-btn class="headline" round flat dark color="pink darken-2">
+    {{ categoryName}}<v-icon right>arrow_forward</v-icon>
+        </v-btn>
+	</v-layout>
+
     <v-layout
       align-center 
       justify-space-between 
@@ -25,12 +30,37 @@
           </v-img>
   
           <v-card-title primary-title>
-            <div>
+	    <v-flex>
 	      <div class="headline">{{ post.title }}</div>
-	      <span> {{ post.deadline }}</span>
-	      <span> {{ post.zip_code}}</span>
-	      <span> {{ post.request_type}}</span>
-            </div>
+	      </v-flex>
+	      <v-layout justify-start row fill-height>
+		<v-flex>
+
+		<v-chip small flat dark color="pink lighten-1">
+
+		    <v-icon small left>calendar_today</v-icon>
+		<span class="font-weight-light">{{ post.deadline | moment("MMMM Do") }} </span>
+
+		</v-chip>
+		</v-flex>
+
+		<v-flex>
+
+		<v-chip dark small flat color="pink lighten-1">
+
+		    <v-icon small left>how_to_reg</v-icon>
+		<span class="font-weight-light"> {{ post.request_type}}</span>
+
+		</v-chip>
+		</v-flex>
+		<v-flex>
+		<v-chip small flat dark color="pink lighten-1">
+		    <v-icon small left>person_pin_circle</v-icon>
+		<span class="font-weight-light"> {{ post.zip_code}}</span>
+		</v-chip>
+		</v-flex>
+	      </v-layout>
+
           </v-card-title>
   
           <v-card-actions>
