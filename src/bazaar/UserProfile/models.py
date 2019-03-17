82da2840294851566a3f5c85bb9c22eef0a9391d
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2019-03-17 19:05:08 -0400

    added email field

--- src/bazaar/UserProfile/models.py
+++ src/bazaar/UserProfile/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import check_password, make_password
 from django.db import models
 
 
@@ -5,7 +6,20 @@ class Profile(models.Model):
     first_name = models.CharField(max_length=200, default='')
     last_name = models.CharField(max_length=200, default='')
     email = models.EmailField()
+    password = models.CharField(max_length=1024, default="defaultPass")
     rating = models.DecimalField(max_digits=3, decimal_places=2)
     description = models.TextField(default='')
     education = models.CharField(max_length=200, default='')
     zip_code = models.IntegerField()
+
+    def create_password(password):
+        """
+        Creates a hashed password of the plaintext passed in
+        """
+        pass
+
+    def login(password):
+        """
+        Tries to login the user with the given password
+        """
+        pass
