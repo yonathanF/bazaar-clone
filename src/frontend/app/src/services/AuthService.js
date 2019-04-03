73e4e7052984a508b637c53995583f39ce324865
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2019-04-03 01:56:37 -0400

    register saves returned token now

--- src/frontend/app/src/services/AuthService.js
+++ src/frontend/app/src/services/AuthService.js
@@ -27,25 +27,29 @@ export function login(email, password) {
     });
 }
 
-export function register(firstname, lastname, email, password){
+export function register(firstname, lastname, email, password) {
   return HTTP.post("auth/create/", {
-      first_name: firstname,
-      last_name: lastname,
-      email: email,
-      password: password,
-      rating: 0.00,
-      description: "",
-      education: "",
-      zip_code: '00000',
+    first_name: firstname,
+    last_name: lastname,
+    email: email,
+    password: password,
+    rating: 0.0,
+    description: "",
+    education: "",
+    zip_code: "00000"
   })
-  .then(handleResponse)
-  .then(response => {
-      return response.data;
-  })
-  .catch(e => {
-    console.log(e);
-    Promise.reject(e);
-  });
+    .then(handleResponse)
+    .then(token => {
+      if (token["token"]) {
+        localStorage.setItem("token", token["token"]);
+      } else if (token["Status"]) {
+        return Promise.reject(token["Status"]);
+      }
+    })
+    .catch(e => {
+      console.log(e);
+      Promise.reject(e);
+    });
 }
 
 export function isAuthenticated() {
