7f776f6c3f8d6413de87d2ef11d6f09748b16e44
Simmy Bhatia <sk8wt@virginia.edu>
2019-04-03 03:51:56 -0400

    removed infinite loop

--- src/bazaar/UserProfile/management/commands/delauth.py
+++ src/bazaar/UserProfile/management/commands/delauth.py
@@ -3,5 +3,4 @@ from UserProfile.models import Authenticator
 
 class Command(BaseCommand):
 	def handle(self, *args, **options):
-		while(1):
-			Authenticator.objects.filter(date_created__week_day__gte=4).delete() #says to delete queries where day of the week = sunday
+		Authenticator.objects.filter(date_created__week_day__gte=4).delete() #says to delete queries where day of the week = sunday
