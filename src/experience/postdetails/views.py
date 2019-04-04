67b81b01b6e2a06e19af82e41fdfba673cd597dc
bkbranden <bk2dh@virginia.edu>
2019-04-03 20:40:44 -0400

    searching finished

--- src/experience/postdetails/views.py
+++ src/experience/postdetails/views.py
@@ -11,20 +11,17 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
 
-logger = logging.getLogger('APPNAME')
 
-from kafka import KafkaProducer
 
 # Create your views here.
 @method_decorator(csrf_exempt, name='dispatch')
 class ShowPostDetails(View):
     def get(self, request, post_id):
         api = APIV1()
-        logger.info(post_id)
         return JsonResponse(api.post_get(post_id), safe=False)
 
     def post(self, request, token):
-        producer = KafkaProducer(bootstrap_servers='kafka:9092')
+        
         data = request.body.decode('utf-8')
         newdata = json.loads(data)
         api = APIV1()
@@ -40,10 +37,12 @@ class ShowPostDetails(View):
         res = api.post_create(title, details, category, preferred_contact,
                               deadline, request_type, zip_code, token)
                  
-        if(res['post'] is not None):
-            prodres = producer.send('new-posts', json.dumps(res).encode('utf-8'))
-            
-            return JsonResponse({'response': res, 'send': str(prodres)}, safe=False)
-        else:
-            return JsonResponse(res, safe=False)
 
+        return JsonResponse(res, safe=False)
+
+class SearchPosts(View):
+
+    def get(self, request, keywords):
+        api = APIV1()
+        results = api.post_search(keywords)
+        return JsonResponse({"posts": results})
