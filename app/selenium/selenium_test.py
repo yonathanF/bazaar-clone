000bac7c9b3a0f0b18e870fdc86904df2f51d443
Simmy Bhatia <sk8wt@virginia.edu>
2019-04-17 01:49:57 -0400

    need to change the selenium test file to work with the web container. Need to update the test file to grab things from the frontend as well

--- app/selenium/selenium_test.py
+++ app/selenium/selenium_test.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--window-size=1420,1080')
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.get(“http://127.0.01:56763/xyz.com”);
+
+
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument('--window-size=1420,1080')
+# chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--disable-gpu')
+# driver = webdriver.Chrome(chrome_options=chrome_options)
