4851adea4b50bb5162e4628322ebddd49bcb921f
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2019-04-17 14:57:10 -0400

    fixed path. It's based on where the script is run from not where the script is

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
-docker-compose -f ../docker-compose-prod.yml build
+docker-compose -f docker-compose-prod.yml build
 echo "Pi3.1415926535" | docker login -u "yonathanf" --password-stdin
 docker push yonathanf/isa_2019_bazaar
