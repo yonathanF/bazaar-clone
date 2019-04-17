dd504e87e4ef973045bab7a88a715133a4cf333f
Simmy Bhatia <sk8wt@virginia.edu>
2019-04-17 01:26:22 -0400

    need to adjust the selenium python script to get it to connect to container name. Need to also figure out where to store python script

--- app/selenium/selenium_test.py
+++ app/selenium/selenium_test.py
@@ -1,12 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--window-size=1420,1080')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(chrome_options=chrome_options)
